1baff544faa9398224f799ba74158866df05fde1
awagnerur <awagnerur@gmail.com>
2016-06-26 19:46:27 -0700

    source format

--- simple_db_sort_demo.php
+++ simple_db_sort_demo.php
@@ -1,65 +1,69 @@
 <!DOCTYPE html>
 <html>
 <style>
-body {
-  font: normal medium/1.4 sans-serif;
-}
-table {
-  border-collapse: collapse;
-  width: 100%;
-}
-th, td {
-  padding: 0.25rem;
-  text-align: left;
-  border: 1px solid #ccc;
-}
-tbody tr:nth-child(odd) {
-  background: #eee;
-}
+	body {
+	  font: normal medium/1.4 sans-serif;
+	}
+	table {
+	  border-collapse: collapse;
+	  width: 100%;
+	}
+	th, td {
+	  padding: 0.25rem;
+	  text-align: left;
+	  border: 1px solid #ccc;
+	}
+	tbody tr:nth-child(odd) {
+	  background: #eee;
+	}
 </style>
 <body>
 
-
 <?php
-$serverName = "localhost";
-$userName = "bluebear_demo1";
-$password = "J00mL@1982";
+	$serverName = "localhost";
+	$userName = "bluebear_demo1";
+	$password = "J00mL@1982";
 
-$users = array();
+	$users = array();
 
-$columnTitle1 = "ID";
-$columnTitle2 = "Name";
-$columnTitle3 = "Company";
+	$columnTitle1 = "ID";
+	$columnTitle2 = "Name";
+	$columnTitle3 = "Company";
 
-$conn = new mysqli($serverName, $userName, $password, "bluebear_demo1");
+	$connection = new mysqli($serverName, $userName, $password, "bluebear_demo1");
 
-if ($conn->connect_errno) {
-	die("Connection failed: " . $conn->connect_error);
-} 
-$tablename = "users";
-$sql = "SELECT id, first_name, last_name, company_id FROM " . $tablename;
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
-		$sql = "SELECT name FROM company WHERE id=" . $row["company_id"];
-		$result2 = $conn->query($sql);
-		$row2 = $result2->fetch_assoc();
-		$users[] = array($columnTitle1 => $row["id"], $columnTitle2 => $row["first_name"]. " " . $row["last_name"], $columnTitle3 => $row2["name"]);
+	if ($connection->connect_errno) {
+		die("Connection failed: " . $connection->connect_error);
 	}
-} else {
-	echo "0 results";
-}
 
-$usersSorted = array();
+	$tableName = "users";
 
-if (isset($_GET['sortBy']))
-{
-	$usersSorted = sortArray($_GET['sortBy'], $users);
-}
-else {
-	$usersSorted = sortArray($columnTitle1, $users);
-}
+	$userQuery = "SELECT id, first_name, last_name, company_id FROM " . $tableName;
+
+	$userQueryResult = $connection->query($userQuery);
+
+	if ($userQueryResult->num_rows > 0) {
+		while($userRow = $userQueryResult->fetch_assoc()) {
+			$companyQuery = "SELECT name FROM company WHERE id=" . $userRow["company_id"];
+			$companyQueryResult = $connection->query($companyQuery);
+			$companyRow = $companyQueryResult->fetch_assoc();
+			$users[] = array($columnTitle1 => $userRow["id"], $columnTitle2 => $userRow["first_name"]. " " . $userRow["last_name"], $columnTitle3 => $companyRow["name"]);
+		}
+	} else {
+		echo "0 results";
+	}
+
+	$usersSorted = array();
+
+	if (isset($_GET['sortBy']))
+	{
+		$usersSorted = sortArray($_GET['sortBy'], $users);
+	}
+	else {
+		$usersSorted = sortArray($columnTitle1, $users);
+	}
 ?>
+
 <table>
 	<thead> 
 		<tr> 
@@ -69,21 +73,24 @@ else {
 		</tr>
 	</thead>
 	<tbody>
-	<?php
 
-	foreach ($usersSorted as $user) {
-		?>
-		<tr>
-			<td><?= $user[$columnTitle1] ?></td>
-			<td><?= $user[$columnTitle2] ?></td>
-			<td><?= $user[$columnTitle3] ?></td>
-		</tr>
-		<?php
-	}?>
+	<?php
+		foreach ($usersSorted as $user) {
+			?>
+			<tr>
+				<td><?= $user[$columnTitle1] ?></td>
+				<td><?= $user[$columnTitle2] ?></td>
+				<td><?= $user[$columnTitle3] ?></td>
+			</tr>
+			<?php
+		}
+	?>
+
 	</tbody>
 </table>
+
 <?php
-$conn->close();
+$connection->close();
 
 function sortArray($sortBy, $arrayToSort) {
 	$keys = array();
